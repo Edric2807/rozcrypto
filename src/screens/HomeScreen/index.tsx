1260826784926ed1c7f6c917d39e76d99ffcca4d
bekrozikoff@gmail.com <bekrozikoff@gmail.com>
2021-09-28 23:06:36 +0500

    Added HomeScreen

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import styles from './styles';
+const image = require('../../../assets/images/Saly-1.png');
 
 const HomeScreen = () => {
 	return (
-		<View>
-			<Text>HomeScreen</Text>
+		<View style={styles.container}>
+			<Image style={styles.image} source={image} />
+			<Text style={styles.header1}>RozCrypto - Find, Track, Invest!</Text>
+			<Text style={styles.header2}>Invest in the future without fear!</Text>
 		</View>
 	);
 };
